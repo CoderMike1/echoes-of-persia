d9c493f6ed87f049353d1ab641694d679dea404d
Mike <mike1@michasmail.com>
2024-05-09 19:18:38 +0200

    Dodanie tileHandlera

---
+++ main/tileHandler.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+
+class TileHandler(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+    def draw(self):
+        pass
+    
+    def update(self):
+        pass
