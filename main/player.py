96a85aa8d1a92b0fd0b3c4e555b573b949a52e7a
Mike <mike1@michasmail.com>
2024-05-17 13:16:27 +0200

    Kolejny

--- main/player.py
+++ main/player.py
@@ -22,16 +22,17 @@ class Player(pygame.sprite.Sprite):
         self.jump = False
         self.jumpCounter = 35
         self.direction = "right"
-        self.walkingCounter =  1
+        self.walkCounter =  1
         self.walk = False
-        self.collision = False
 
-    def pLoad(self,fileName,flip=False):
+
+        self.crouch = False
+        self.crouchCounter = 1
+
+    def pLoad(self,fileName):
         i = pygame.image.load(os.path.join(self.path,fileName+".png")).convert_alpha()
         i = pygame.transform.scale(i,(i.get_width()*self.PLAYER_SCALE,i.get_height()*self.PLAYER_SCALE))
-        flip = False
-        if flip:
-            i = pygame.transform.flip(i,True,False)
+
 
         return i
     def loadImages(self):
@@ -48,102 +49,124 @@ class Player(pygame.sprite.Sprite):
         im.update({"walk9": self.pLoad("walk9")})
         im.update({"walk10": self.pLoad("walk10")})
         im.update({"walk11": self.pLoad("walk11")})
+
+        im.update({"crouch1":self.pLoad("crouch1")})
+        im.update({"crouch2": self.pLoad("crouch2")})
+        im.update({"crouch3": self.pLoad("crouch3")})
+        im.update({"crouch4": self.pLoad("crouch4")})
+        im.update({"crouch5": self.pLoad("crouch5")})
+        im.update({"crouch6": self.pLoad("crouch6")})
+        im.update({"crouch7": self.pLoad("crouch7")})
+        im.update({"crouch8": self.pLoad("crouch8")})
+        im.update({"crouch9": self.pLoad("crouch9")})
+        im.update({"crouch10": self.pLoad("crouch10")})
+        im.update({"crouch11": self.pLoad("crouch11")})
         return im
 
     def draw(self,surface):
-
         #pygame.draw.rect(surface,(0,0,0,0),self.rect)
         surface.blit(self.image,self.rect)
 
     def update(self):
-        #aktualne polozenie na prawo i lewo
-        current_right = self.rect.right
-        current_left = self.rect.left
+        #reset oraz dodawanie do wartosci
+        self.vel_x = 0
+        self.jumpCounter +=1
         self.walk = False
-        self.jumpCounter+=1
-        self.vel_x = self.SPEED
-        self.vel_y += 1
+        current_left = self.rect.left
+        current_right = self.rect.right
+        current_bottom = self.rect.bottom
+        current_top = self.rect.top
+
+
+        #grawitacja
+        self.vel_y +=1
         if self.vel_y > 10:
             self.vel_y = 10
 
         keys = pygame.key.get_pressed()
-        for tile in self.game.tileHandler.tileMap:
-
-            #gora
-            if tile.rect.colliderect(self.rect.x, self.rect.y - self.SPEED*2, self.rect.width,self.rect.height):
-                if self.rect.top - self.SPEED < tile.rect.bottom:
-                    print(self.rect.top)
-                    print(tile.rect.bottom)
-                    print('kolizo')
-                    self.rect.top = tile.rect.bottom
-                    self.vel_y +=1
 
 
+        #poruszanie sie postacia
+        if keys[pygame.K_RIGHT]:
+            self.direction = "right"
+            self.walkCounter += 0.1
+            if self.walkCounter > 11:
+                self.walkCounter = 3
+            self.image = pygame.transform.flip(self.images[f"walk{int(self.walkCounter)}"],True,False)
+            self.rect = self.image.get_rect(right=current_right,bottom=current_bottom)
+            self.walk = True
+            self.vel_x = self.SPEED
 
-            #dół
-            if tile.rect.colliderect(self.rect.x, self.rect.y + self.SPEED, self.rect.width, self.rect.height):
-                if self.vel_y >= 0:
-                    self.vel_y = 0
+        if keys[pygame.K_LEFT]:
+            self.direction = "left"
+            self.walkCounter += 0.1
+            if self.walkCounter >11:
+                self.walkCounter = 3
+            self.image = self.images[f"walk{int(self.walkCounter)}"]
+            self.rect = self.image.get_rect(right=current_right,bottom=current_bottom,left=current_left)
+
+            self.walk = True
+            self.vel_x = -self.SPEED
+
+        #jesli postac przestanie isc
+        if not self.walk:
+            self.walkCounter = 1
+
+            if self.direction == "right":
+                self.image = pygame.transform.flip(self.images[f"idle1"], True, False)
+                self.rect = self.image.get_rect(topright=(current_right, current_top), topleft=(current_left,current_top))
+            else:
+                self.image = self.images[f"idle1"]
+                self.rect = self.image.get_rect(topleft=(current_left, current_top),topright=(current_right, current_top))
 
 
 
-            #prawa strona
-            if tile.rect.colliderect(self.rect.x + self.SPEED, self.rect.y-20, self.rect.width, self.rect.height):
-                if keys[pygame.K_RIGHT] and self.rect.right + self.SPEED > tile.rect.left:
-                    self.vel_x = 0
 
-
-            #lewa strona
-            if tile.rect.colliderect(self.rect.x - self.SPEED, self.rect.y -20, self.rect.width, self.rect.height):
-                if keys[pygame.K_LEFT] and self.rect.left - self.SPEED < tile.rect.right:
-                    self.vel_x = 0
-
-
-        self.rect.y += self.vel_y
-
-
+        #skok
         if keys[pygame.K_SPACE] and not self.jump and self.jumpCounter > 35:
             self.vel_y = -self.SPEED * 2.5
             self.jumpCounter = 0
             self.jump = True
         if not keys[pygame.K_SPACE]:
             self.jump = False
+        for tile in self.game.tileHandler.tileMap:
+            f_recY = pygame.Rect(self.rect.x,self.rect.y + self.vel_y,self.rect.width,self.rect.height)
+            if tile.rect.colliderect(f_recY):
 
-        if keys[pygame.K_LEFT]:
-            self.direction = "left"
-            self.walkingCounter += 0.1
-            if self.walkingCounter > 11:
-                self.walkingCounter = 3
-            self.image = self.images[f"walk{int(self.walkingCounter)}"]
-            self.rect = self.image.get_rect(topleft=(current_left, self.rect.top))
-            self.rect.x -= self.vel_x
-            self.walk = True
+                if self.vel_y < 0:
+                    #skacze
+                    self.rect.top = tile.rect.bottom
+                    self.vel_y = 0
 
-
-        elif keys[pygame.K_RIGHT]:
-            self.direction = "right"
-            self.walkingCounter += 0.1
-            if self.walkingCounter > 11:
-                self.walkingCounter = 3
-            self.image = pygame.transform.flip(self.images[f"walk{int(self.walkingCounter)}"], True,
-                                               False)
-            self.rect = self.image.get_rect(topright=(current_right, self.rect.top))
-            self.rect.x += self.vel_x
-            self.walk = True
-
-        if not self.walk:
-            self.walkingCounter = 1
-
-            if self.direction == "right":
-                self.image = pygame.transform.flip(self.images[f"idle1"],True,False)
-                self.rect = self.image.get_rect(topright=(current_right, self.rect.top))
-            else:
-                self.image = self.images[f"idle1"]
-                self.rect = self.image.get_rect(topleft=(current_left, self.rect.top))
+                elif self.vel_y >0:
+                    #spada
+                    self.rect.bottom = tile.rect.top
+                    self.vel_y = 0
 
 
-        # if keys[pygame.K_s]:
-        #     self.rect.move_ip([0, SPEED])
+            f_recX = pygame.Rect(self.rect.x + self.vel_x,self.rect.y, self.rect.width,self.rect.height)
+            if tile.rect.colliderect(f_recX):
+                if self.vel_x > 0:
+                    # w prawo
+                    self.rect.right = tile.rect.left
+                    self.vel_x = 0
+
+                elif self.vel_x <0:
+                    #w lewo
+                    self.rect.left = tile.rect.right
+                    self.vel_x = 0
+
+
+
+
+        #ruch
+        self.rect.x += self.vel_x
+        self.rect.y += self.vel_y
+
+
+
+
+
 
 
 
--- main/tileHandler.py
+++ main/tileHandler.py
@@ -5,11 +5,13 @@ import pygame,os
 class Tile(pygame.sprite.Sprite):
     pathTiles = os.path.join(os.path.dirname(os.getcwd()), 'images','tiles')
 
-    def __init__(self,image,c_x,c_y):
+    def __init__(self,image,c_x,c_y,col,row):
         super().__init__()
         self.image = pygame.image.load(os.path.join(self.pathTiles,image)).convert()
         self.rect = self.image.get_rect()
         self.rect.center = c_x,c_y
+        self.row = row
+        self.col = col
 
     def draw(self,surface):
         surface.blit(self.image,self.rect)
@@ -37,7 +39,7 @@ class TileHandler(pygame.sprite.Sprite):
         for x in map:
             for y in x:
                 if y == '1':
-                    self.tileMap.append(Tile('tile1.png', col*self.game.PIXEL_SIZE,row*self.game.PIXEL_SIZE))
+                    self.tileMap.append(Tile('tile1.png', col*self.game.PIXEL_SIZE,row*self.game.PIXEL_SIZE,col,row))
                     #self.all_sprites.add(Tile('tile1.png', col*self.game.PIXEL_SIZE,row*self.game.PIXEL_SIZE))
                 col +=1
 
